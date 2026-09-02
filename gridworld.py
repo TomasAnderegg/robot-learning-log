c1dde0a97486ed50bcacbbac50dfded919bc9cd1
Tomas Anderegg <tjga.98@gmail.com>
2026-09-02 21:18:59 +0200

    agent deployment

--- gridworld.py
+++ gridworld.py
@@ -15,7 +15,7 @@ class Gridworld:
         self.goal = 10
         self.trap = -10
         self.map = np.full((size,size), -1)
-
+         
         self.map[np.random.randint(0,size),np.random.randint(0,size)] = self.goal
 
         #Can be optimized
@@ -30,15 +30,32 @@ class Gridworld:
             j = np.random.randint(0,size)
 
         self.map[i,j] = self.trap
+        self.agent_pos = (0,0)
 
-
+ 
+    
     def gridconst(self):
         # pass
         print(self.map)
         print(np.random.randint(self.size))
-        
-        
+
+    def reset(self):
+        pass
+
+class Agent:
+    def __init__(self,size=4):
+        self.actions = ['up', 'down', 'left', 'right']
+        self.start_pos = (np.random.randint(0,size),np.random.randint(0,size))
+        self.curr_state = self.start_pos
+        self.q_values = []
+        self.v_values = []
+        self.reward = 0
+
 if __name__ == "__main__":        
 
-    grid = Gridworld()
-    # grid.gridconst()
+    #World 
+    grid = Gridworld(size=5)
+    grid.gridconst()
+
+    #Agent
+    agent = Agent(size=5)
